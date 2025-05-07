5e5a3e48902bf3dd2e578b6e6f6395b9358cea64
Steven Lopez Mancera <150303550+StevenLopez009@users.noreply.github.com>
2025-05-07 15:08:00 -0500

    mensaje para despliegue

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { connectDB } from "./db.js";
 connectDB();
 const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.send('¡Hola desde mi backend!');
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
